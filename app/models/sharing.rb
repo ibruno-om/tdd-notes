3da1957b67d21b898a931f2d5e0864de21d788e3
Iago Oliveira <ibruno.om@gmail.com>
2021-09-27 23:25:38 -0300

    Added schema info to sharing model

--- app/models/sharing.rb
+++ app/models/sharing.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: sharings
+#
+#  id         :bigint           not null, primary key
+#  note_id    :bigint           not null
+#  user_id    :bigint           not null
+#  permission :integer          default("view")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Sharing < ApplicationRecord
   # Associations
   belongs_to :note, required: true
--- spec/factories/sharings.rb
+++ spec/factories/sharings.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: sharings
+#
+#  id         :bigint           not null, primary key
+#  note_id    :bigint           not null
+#  user_id    :bigint           not null
+#  permission :integer          default("view")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :sharing do
     note { nil }
